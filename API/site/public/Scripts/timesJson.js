5f12c8dfa26b609b519f8b2eb98d163ff8012994
Leozinho11 <leoferraribarreto@hotmail.com>
2023-05-23 14:48:06 -0300

    CPF input Cleave.js mask added

---
+++ API/site/public/Scripts/timesJson.js
@@ -0,0 +1,12 @@
+var AmericaMG = {
+    dataFund: '1912-04-30',
+    cidadeNatal: 'Belo Horizonte',
+    estadio: 'Arena Independência',
+    titulos: {
+        serieB: 2,
+        serieC: 1,
+        estadual: 16
+    },
+    rebaixamentos: 6,
+    historia: ''
+}
